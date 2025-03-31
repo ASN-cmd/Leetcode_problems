9ff449381f097ae81639d6609c559cf26be370ab
Aryan S Nair <140100567+ASN-cmd@users.noreply.github.com>
2025-03-31 09:58:15 +0530

    Time: 0 ms (100%), Space: 45.5 MB (89.21%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,17 +1,23 @@
+//Recursive Approach
+
 class Solution {
-    public int search(int[] nums, int target) {
-        int n = nums.length;
-        int low = 0, high = n-1;
+    public static int binarySearch(int[] nums,int low, int high, int target){
+        if(low > high)
+            return -1;
         
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] > target)
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        return -1;
+        int mid = (low + high)/2;
+
+        if(nums[mid] == target)
+            return mid;
+
+        if(nums[mid] < target)
+            return binarySearch(nums, mid+1, high, target);
+        else
+            return binarySearch(nums, low, mid-1, target);
     }
+
+    public int search(int[] nums, int target) {
+        return binarySearch(nums,0, nums.length - 1, target);
+
+}
 }
